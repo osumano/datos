4e4e37253921070ffd4816179fca511531c81744
Oscar Sumano <osumano@gmail.com>
2019-11-11 13:41:14 -0500

    removed run from it

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 from apiv1 import blueprint as apiv1
 
 app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
 app.register_blueprint(apiv1)
-app.run(debug=True)
