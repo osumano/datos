6d549f06d33c5c6a47727ef681e86fc69645b704
Oscar Sumano <osumano@gmail.com>
2019-11-11 08:23:22 -0500

    Need to address the title, version and description when feeding in the blueprint to API

--- apiv1.py
+++ apiv1.py
@@ -6,10 +6,6 @@ api = Api(blueprint)
 from .apis.clusters import api as clusters
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
-api = Api(blueprint,
-    title='Datos API',
-    version='1.0',
-    description='v1 of the Datos API',
-)
+api = Api(blueprint)
 
 api.add_namespace(clusters)
