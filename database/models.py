9d6f28536bde26c95fbc7a68ebbf7784907d3d34
Oscar Sumano <osumano@gmail.com>
2019-11-11 13:42:03 -0500

    initial database model

--- database/models.py
+++ database/models.py
@@ -1,25 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import datos
+from datos.app import app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = centralapi.app.config["postgres_local_base"]
+
 db = SQLAlchemy(app)
-
-
 
 
 class Cluster(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), nullable=False)
     asset = db.Column(db.String(256), nullable=False)
-    environment = db.Column(db.String(256), nullable=False)
-    oidc_endpoint = db.Column(db.String(256), nullable=False)
     control_plane_label = db.Column(db.String(256), nullable=False)
     dns_subdomain = db.Column(db.String(256), nullable=False)
-    node_id = db.Column(node.Integer, db.ForeignKey('node.id'),
+    node_id = db.Column(db.Integer, db.ForeignKey('node.id'),
        nullable=False)
-       
+
     node = db.relationship('Node',
         backref=db.backref('clusters', lazy=True))
 
