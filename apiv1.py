f72ee20e2bdef896a575a5fff10da43ed3fa40df
Oscar Sumano <osumano@gmail.com>
2019-11-11 08:14:04 -0500

    fixed issue broken messages

--- apiv1.py
+++ apiv1.py
@@ -6,7 +6,7 @@ api = Api(blueprint)
 from .apis.clusters import api as clusters
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
-api = Api(blueprint
+api = Api(blueprint,
     title='Datos API',
     version='1.0',
     description='v1 of the Datos API',
