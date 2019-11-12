b16f62c5d23b6a970aacd4df5c40fea13c0624f0
Oscar Sumano <osumano@gmail.com>
2019-11-11 22:43:02 -0500

    added new namespace and model for Nodes

--- apiv1.py
+++ apiv1.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from flask_restplus import Api
 from apis.clusters import api as clusters
+from apis.nodes import api as nodes
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(blueprint)
 
 api.add_namespace(clusters)
+api.add_namespace(nodes)
--- db_init.py
+++ db_init.py
@@ -1,9 +1,12 @@
 from database.models import db
 from database.models import Cluster
+from database.models import Node
 
 db.create_all()
 
 cluster1 = Cluster(name='cluster1', asset='grn:/sumano', control_plane_label='cluster1',dns_subdomain='sumano.kod.ms.com')
-
+node1 = Node(name='node1',hostname='node1.ms.com',cluster_id=1)
 db.session.add(cluster1)
+db.session.add(node1)
+
 db.session.commit()
