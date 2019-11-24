d0ca01533be01da2e7fe0aec0bc290a50a73cf27
Oscar Sumano <osumano@gmail.com>
2019-11-24 12:28:32 -0500

    added update method

--- apis/clusters.py
+++ apis/clusters.py
@@ -22,7 +22,7 @@ class ClusterList(Resource):
         '''List all clusters'''
         return Cluster.query.all()
 
-    @api.doc('create_todo')
+    @api.doc('create cluster')
     @api.expect(cluster)
     @api.marshal_with(cluster, skip_none=True)
     def post(self):
@@ -46,6 +46,37 @@ class ClusterList(Resource):
             db.session.add(new_cluster)
             db.session.commit()
             return req_data, 201
+
+    @api.doc('update cluster ')
+    @api.expect(cluster)
+    @api.marshal_with(cluster, skip_none=True)
+    def put(self):
+        '''Update a  cluster'''
+        req_data = request.get_json()
+        required = ['name', 'asset', 'control_plane_label', 'dns_subdomain']
+        if not all(value in req_data for value in required):
+            return 'Missing values', 400
+        n_name = req_data['name']
+        n_asset = req_data['asset']
+        n_control_plane_label = req_data['control_plane_label']
+        n_dns_subdomain = req_data['dns_subdomain']
+        if None  in (n_name, n_asset, n_control_plane_label, n_dns_subdomain):
+            return None, 422  #user didnt provide all the fields
+
+        check_cluster = Cluster.query.filter_by(name=n_name).first()
+        if check_cluster:
+            update_cluster = Cluster.query.filter_by(name=n_name).first()
+            update_cluster.name = n_name
+            update_cluster.asset = n_asset
+            update_cluster.control_plane_label = n_control_plane_label
+            update_cluster.dns_subdomain = n_dns_subdomain
+            db.session.commit()
+            return req_data, 200
+        else:
+            return None, 404
+
+
+
 
 #GET by cluster name and DELETE by cluster name
 @api.route('/<name>')
