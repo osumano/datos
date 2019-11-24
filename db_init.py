00d06780a041826b79a018f867710b95eebf2a66
Oscar Sumano <osumano@gmail.com>
2019-11-24 11:27:28 -0500

    updated examples

--- db_init.py
+++ db_init.py
@@ -4,8 +4,8 @@ from database.models import Node
 
 db.create_all()
 
-cluster1 = Cluster(name='cluster1', asset='grn:/sumano', control_plane_label='cluster1',dns_subdomain='sumano.kod.ms.com')
-node1 = Node(name='node1',hostname='node1.ms.com',cluster_id=1)
+cluster1 = Cluster(name='cluster1', asset='appTeam1', control_plane_label='cluster1_appteam1',dns_subdomain='mydomain.testdomain.com')
+node1 = Node(name='node1',hostname='node1.testdomain.com',cluster_id=1)
 db.session.add(cluster1)
 db.session.add(node1)
 
